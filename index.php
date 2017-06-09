b5b92e5db84f000a50223f22de642287febaebb2
Emerson <emerson@wearefine.com>
2017-06-09 13:10:59 -0700

    /w endpoint

--- index.php
+++ index.php
@@ -20,14 +20,27 @@ $app->get('/c/{n}', function ($request, $response, $args) {
 
 	include './mobydick1-8.php';
 				# start with a capital letter.   # set length of following chars
-	$pattern = '/[ABCDEFGHIJKLMNOPQRS].{' 		. ( intval($args['n']) -2 ) . '}\./';
+	$pattern = '/[ABCDEFGHIJKLMNOPQRSTUVWXYZ].{' 		. ( intval($args['n']) -2 ) . '}\./';
 	preg_match_all($pattern, $mobydick1_8, $matches);
 
 	# if no matches with period at the end, try without
 	if ( empty($matches[0]) ) {
-		$pattern2 = '/[ABCDEFGHIJKLMNOPQRS].{' 		. ( intval($args['n']) -1 ) . '}/';
+		$pattern2 = '/[ABCDEFGHIJKLMNOPQRSTUVWXYZ].{' 		. ( intval($args['n']) -1 ) . '}/';
 		preg_match_all($pattern2, $mobydick1_8, $matches);
 	}
+
+	$i = ( !empty($matches) && !empty($matches[0]) ) ? mt_rand( 0, count($matches[0])-1 )  : 0;
+
+    return $response->write($matches[0][$i]);
+});
+
+# Word count endpoint
+$app->get('/w/{n}', function ($request, $response, $args) {
+
+	include './mobydick1-8.php';
+				# start with a capital letter.   # set length of following chars
+	$pattern = '/[ABCDEFGHIJKLMNOPQRSTUVWXYZ](\S+[\s]){' 		. intval($args['n']) . '}/';
+	preg_match_all($pattern, $mobydick1_8, $matches);
 
 	$i = ( !empty($matches) && !empty($matches[0]) ) ? mt_rand( 0, count($matches[0])-1 )  : 0;
 
