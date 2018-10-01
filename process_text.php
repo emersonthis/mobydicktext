0cfbd9b74e364da4344e14648921f7a2315c5406
Emerson Loustau <e@thesecretaddress.com>
2018-09-30 21:36:37 -0700

    words.php characters.php

---
+++ process_text.php
@@ -0,0 +1,45 @@
+<?php
+
+include 'mobydick_wo_chapters.php';
+
+$matches = [];
+
+preg_match_all('/[A-Z][^\.!\?]*[\.!\?]/', $mobydick, $matches);
+
+$characters = [];
+$words = [];
+
+foreach( $matches[0] as $match ) {
+
+    // add to characters
+    $length = strlen($match);
+
+    if (isset($words[$length])) {
+        $characters[$length][] = $match;
+    } else {
+        $characters[$length] = [];
+        $characters[$length][] = $match;
+    }
+
+
+    // add to words
+
+    $wordCount = str_word_count($match);
+
+    if (isset($words[$wordCount])) {
+        $words[$wordCount][] = $match;
+    } else {
+        $words[$wordCount] = [];
+        $words[$wordCount][] = $match;
+    }
+
+
+}
+
+ksort($characters);
+ksort($words);
+
+// var_dump($words);
+
+file_put_contents('characters.php', '<?php $characters = ' . var_export($characters, true));
+file_put_contents('words.php', '<?php $words = ' . var_export($words, true));
